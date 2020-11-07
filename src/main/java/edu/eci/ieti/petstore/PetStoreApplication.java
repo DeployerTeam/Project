2a23541ca5525b5ec41a96eb0ef38b650474dbc5
CAndresRa <carlos.ramirez-t@mail.escuelaing.edu.co>
2020-11-06 20:09:16 -0500

    deploy

--- src/main/java/edu/eci/ieti/petstore/PetStoreApplication.java
+++ src/main/java/edu/eci/ieti/petstore/PetStoreApplication.java
@@ -4,10 +4,22 @@ package edu.eci.ieti.petstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class PetStoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PetStoreApplication.class, args);
+
+        SpringApplication app = new SpringApplication(PetStoreApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+        app.run(args);
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 
 }

e20e2eedcfcfddbfea97953cf0fa1161dba28be5
CAndresRa <carlos.ramirez-t@mail.escuelaing.edu.co>
2020-11-06 20:27:01 -0500

    cors

--- src/main/java/edu/eci/ieti/petstore/controllers/VeterinaryController.java
+++ src/main/java/edu/eci/ieti/petstore/controllers/VeterinaryController.java
@@ -2,10 +2,12 @@ package edu.eci.ieti.petstore.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/veterinary")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class VeterinaryController {
 
 }
